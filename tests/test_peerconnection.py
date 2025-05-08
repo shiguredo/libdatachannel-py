b294df77ffb6e18f5dbcf0f30b99cf4c5c6c6db2
melpon <shigemasa7watanabe+github@gmail.com>
2025-05-09 04:53:11 +0900

    リークするテスト追加

--- tests/test_peerconnection.py
+++ tests/test_peerconnection.py
@@ -194,3 +194,102 @@ def test_track():
     assert t2[0].is_closed()
 
     print("Success")
+
+
+# test_track() と同じようなことをするけど、バインドしたまま close せずに終了する
+def test_leak():
+    config1 = Configuration()
+    pc1 = PeerConnection(config1)
+
+    config2 = Configuration()
+    config2.port_range_begin = 5000
+    config2.port_range_end = 6000
+    pc2 = PeerConnection(config2)
+
+    def pc1_on_local_description(desc):
+        print("Description 1: " + str(desc))
+        pc2.set_remote_description(Description(str(desc)))
+
+    def pc1_on_local_candidate(candidate):
+        print("Candidate 1: " + str(candidate))
+        pc2.add_remote_candidate(Candidate(str(candidate)))
+
+    def pc1_on_state_change(state):
+        print("State 1: " + str(state))
+
+    def pc1_on_gathering_state_change(state):
+        print("Gathering state 1: " + str(state))
+
+    pc1.on_local_description(pc1_on_local_description)
+    pc1.on_local_candidate(pc1_on_local_candidate)
+    pc1.on_state_change(pc1_on_state_change)
+    pc1.on_gathering_state_change(pc1_on_gathering_state_change)
+
+    def pc2_on_local_description(desc):
+        print("Description 2: " + str(desc))
+        pc1.set_remote_description(Description(str(desc)))
+
+    def pc2_on_local_candidate(candidate):
+        print("Candidate 2: " + str(candidate))
+        pc1.add_remote_candidate(Candidate(str(candidate)))
+
+    def pc2_on_state_change(state):
+        print("State 2: " + str(state))
+
+    def pc2_on_gathering_state_change(state):
+        print("Gathering state 2: " + str(state))
+
+    pc2.on_local_description(pc2_on_local_description)
+    pc2.on_local_candidate(pc2_on_local_candidate)
+    pc2.on_state_change(pc2_on_state_change)
+    pc2.on_gathering_state_change(pc2_on_gathering_state_change)
+
+    t2 = [None]
+    new_track_mid = ""
+
+    def pc2_on_track(t):
+        mid = t.mid()
+        print(f'Track 2: Received track with mid "{mid}"')
+        if mid != new_track_mid:
+            print("Wrong track mid", file=sys.stderr)
+            return
+
+        def t_on_open():
+            print(f'Track 2: Track with mid "{mid}" is open')
+
+        def t_on_closed():
+            print(f'Track 2: Track with mid "{mid}" is closed')
+
+        t.on_open(t_on_open)
+        t.on_closed(t_on_closed)
+        t2[0] = t
+
+    pc2.on_track(pc2_on_track)
+
+    # Test opening a track
+    new_track_mid = "test"
+
+    media = Description.Video(new_track_mid, Description.Direction.SendOnly)
+    media.add_h264_codec(96)
+    media.set_bitrate(3000)
+    media.add_ssrc(1234, "video-send")
+
+    media_sdp1 = str(media)
+    media_sdp2 = str(Description.Media(media_sdp1))
+    assert media_sdp1 == media_sdp2
+
+    t1 = pc1.add_track(media)
+
+    pc1.set_local_description()
+
+    attempts = 10
+    while (not t1.is_open() or t2[0] is None or not t2[0].is_open()) and attempts > 0:
+        attempts -= 1
+        time.sleep(1)
+
+    assert pc1.state() == PeerConnection.State.Connected
+    assert pc2.state() == PeerConnection.State.Connected
+
+    assert t1.is_open()
+    assert t2[0] is not None
+    assert t2[0].is_open()
