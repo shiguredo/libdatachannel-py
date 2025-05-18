e7c6b8ace08c3095c82ae03724831b891417925c
melpon <shigemasa7watanabe+github@gmail.com>
2025-05-19 01:39:18 +0900

    修正

--- tests/test_video_codec.py
+++ tests/test_video_codec.py
@@ -144,8 +144,8 @@ def test_openh264():
     success = encoder.init(settings)
     assert success
     frame = VideoFrame()
-    frame.i420_buffer = VideoFrameBufferI420.create(640, 360)
-    frame.format = ImageFormat.I420
+    frame.i420_buffer = VideoFrameBufferNV12.create(640, 360)
+    frame.format = ImageFormat.NV12
     frame.base_width = 640
     frame.base_height = 480
     frame.timestamp = timedelta(microseconds=1234567)
