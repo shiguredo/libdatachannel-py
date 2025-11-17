16e73562664adaaf0f7f13e9f374ff53657d5f2d
voluntas <nakai@shiguredo.jp>
2025-11-17 22:24:57 +0900

    scikit-build-core へ移行する

--- src/openh264_video_encoder.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBDATACHANNEL_OPENH264_VIDEO_ENCODER_H_INCLUDED
-#define LIBDATACHANNEL_OPENH264_VIDEO_ENCODER_H_INCLUDED
-
-#include "video_codec.h"
-
-std::shared_ptr<VideoEncoder> CreateOpenH264VideoEncoder(std::string openh264);
-
-#endif
--- src/videotoolbox_video_encoder.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBDATACHANNEL_VIDEOTOOLBOX_VIDEO_ENCODER_H_INCLUDED
-#define LIBDATACHANNEL_VIDEOTOOLBOX_VIDEO_ENCODER_H_INCLUDED
-
-#include "video_codec.h"
-
-std::shared_ptr<VideoEncoder> CreateVideoToolboxVideoEncoder();
-
-#endif
